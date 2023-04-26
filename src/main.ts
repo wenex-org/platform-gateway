1459e858368824afe43ebb8dacead1cdbc912bae
Vahid V <vhid.vz@gmail.com>
2023-04-26 19:48:02 +0330

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { NamingConventionsInterceptor } from '@app/common/interceptors';
 import { GatewayModule } from './gateway.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NODE_ENV } from '@app/common/configs';
+import { NestFactory } from '@nestjs/core';
+import { initTracing } from 'tracing';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(GatewayModule);
+  if (NODE_ENV().IS_PRODUCTION) await initTracing();
+
+  const app = await NestFactory.create(GatewayModule, { cors: true });
+
+  app.useGlobalInterceptors(new NamingConventionsInterceptor());
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
