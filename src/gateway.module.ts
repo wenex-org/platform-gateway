5600595e60681dc2c526e35c255e9ca4cad4fe40
Vahid V <vhid.vz@gmail.com>
2023-05-20 01:11:18 +0330

    update

--- src/gateway.module.ts
+++ src/gateway.module.ts
@@ -1,8 +1,6 @@
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
-import { DirectiveLocation, GraphQLDirective } from 'graphql';
-import { upperDirectiveTransformer } from './common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { HealthModule } from '@app/health';
 import { Module } from '@nestjs/common';
@@ -18,18 +16,8 @@ import { join } from 'path';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       playground: false,
       driver: ApolloDriver,
-      installSubscriptionHandlers: true,
       autoSchemaFile: join(process.cwd(), 'schema.gql'),
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
-      transformSchema: (schema) => upperDirectiveTransformer(schema, 'upper'),
-      buildSchemaOptions: {
-        directives: [
-          new GraphQLDirective({
-            name: 'upper',
-            locations: [DirectiveLocation.FIELD_DEFINITION],
-          }),
-        ],
-      },
     }),
   ],
 })
--- src/modules/primary/identity/sessions/sessions.resolver.ts
+++ src/modules/primary/identity/sessions/sessions.resolver.ts
@@ -1,15 +1,53 @@
-import { CountSerializer } from '@app/common/serializers';
+import {
+  ClassSerializerInterceptor,
+  UseFilters,
+  UseGuards,
+  UseInterceptors,
+  UsePipes,
+} from '@nestjs/common';
+import {
+  GqlAuthorityInterceptor,
+  MetadataTakeInterceptor,
+  RateLimitInterceptor,
+} from '@app/common/interceptors';
+import { CountSerializer, SessionSerializer } from '@app/common/serializers';
+import { AuthGuard, PolicyGuard, ScopeGuard } from '@app/common/guards';
+import { Resource, Scope, SysAction } from '@app/common/enums';
+import { SetPolicy, SetScope } from '@app/common/metadatas';
+import { AllExceptionsFilter } from '@app/common/filters';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Filter, Meta } from '@app/common/decorators';
+import { ValidationPipe } from '@app/common/pipes';
+import { CountFilterDto } from '@app/common/dto';
+import { toRaw } from '@app/common/utils';
+import { Metadata } from '@grpc/grpc-js';
+import { lastValueFrom } from 'rxjs';
 
 import { SessionsProvider } from './sessions.provider';
-import { Query, Resolver } from '@nestjs/graphql';
 
-@Resolver(() => CountSerializer)
+@UsePipes(ValidationPipe)
+@UseFilters(AllExceptionsFilter)
+@Resolver(() => SessionSerializer)
+@UseInterceptors(RateLimitInterceptor)
+@UseGuards(AuthGuard, ScopeGuard, PolicyGuard)
+@UseInterceptors(
+  GqlAuthorityInterceptor,
+  MetadataTakeInterceptor,
+  ClassSerializerInterceptor,
+)
 export class SessionsResolver {
   constructor(private readonly provider: SessionsProvider) {}
 
   @Query(() => CountSerializer)
-  async total(): Promise<CountSerializer> {
-    console.log(0);
-    return CountSerializer.build(10);
+  @SetScope(Scope.ReadIdentitySessions)
+  @SetPolicy(SysAction.Read, Resource.IdentitySessions)
+  async countSession(
+    @Meta() meta: Metadata,
+    @Filter() @Args('filter') filter: CountFilterDto,
+  ): Promise<CountSerializer> {
+    return CountSerializer.build(
+      (await lastValueFrom(this.provider.service.count(toRaw(filter), meta)))
+        .count,
+    );
   }
 }
